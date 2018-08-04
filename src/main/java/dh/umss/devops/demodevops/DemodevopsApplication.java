0fda027b25ceb6cb25a2351a79bb1826bdf3e1fd
jpmejia <cromeocromeo@gmail.com>
2018-08-03 22:17:11 -0400

    init proyect spring

---
+++ src/main/java/dh/umss/devops/demodevops/DemodevopsApplication.java
@@ -0,0 +1,12 @@
+package dh.umss.devops.demodevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodevopsApplication.class, args);
+	}
+}
---
+++ src/test/java/dh/umss/devops/demodevops/DemodevopsApplicationTests.java
@@ -0,0 +1,16 @@
+package dh.umss.devops.demodevops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemodevopsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
